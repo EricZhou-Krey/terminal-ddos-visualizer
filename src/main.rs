2c01f955e9ed25d6e1f48c88f95abbc1a5d62589
Eric Zhou <ericzhou1101@gmail.com>
2025-12-12 16:48:27 +0000

    ddos attack getting functional

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,48 @@
+use std::time::SystemTime;
+
+use chrono::{DateTime, TimeDelta, Utc};
+use reqwest::{
+    header::{HeaderValue, AUTHORIZATION},
+    Client,
+};
+
+async fn get_content() -> String {
+    let token: String = match std::env::var("CLOUDFLARE_API_KEY") {
+        Ok(val) => val,
+        Err(e) => {
+            println!("Cannot find cloudflare API key, {}", e);
+            "".to_string()
+        }
+    };
+
+    let client: Client = Client::new();
+    let auth_value =
+        HeaderValue::from_str(&format!("Bearer {}", token)).expect("Invalid API Token format");
+
+    let end_time: DateTime<Utc> = SystemTime::now().into();
+    let start_time: DateTime<Utc> = end_time - TimeDelta::minutes(360);
+
+    let layer: &str = "7";
+    let protocol: &str = "http_method";
+
+    let endpoint_url = format!("https://api.cloudflare.com/client/v4/radar/attacks/layer{layer}/timeseries_groups/{protocol}?dateStart={}&dateEnd={}&resolution=5m",      
+        start_time.format("%Y-%m-%dT%H:%M:00Z"),
+        end_time.format("%Y-%m-%dT%H:%M:00Z")
+        );
+
+    let response = client
+        .get(endpoint_url)
+        .header(AUTHORIZATION, auth_value)
+        .send()
+        .await;
+
+    if let Ok(result) = response {
+        result.text().await.unwrap_or("".to_string())
+    } else {
+        "".to_string()
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("{}", trpl::block_on(get_content()))
 }
