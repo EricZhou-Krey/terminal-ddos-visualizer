6c600d6b5860dfe237ae77b18856a361acaaf58b
Eric Zhou <ericzhou1101@gmail.com>
2025-12-14 15:24:26 +0000

    added display to terminal for ratatui

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use std::{collections::VecDeque, time::SystemTime};
+use std::{
+    collections::{HashMap, VecDeque},
+    time::SystemTime,
+};
 
 use chrono::{DateTime, TimeDelta, Utc};
 use reqwest::{
@@ -9,7 +12,18 @@ use reqwest::{
 use serde::Deserialize;
 
 use crossterm::event::{self, Event};
-use ratatui::{DefaultTerminal, Frame};
+use ratatui::{
+    buffer::Buffer,
+    layout::{self, Constraint, Direction, Layout, Rect},
+    style::{Color, Stylize},
+    symbols::Marker,
+    text::Line,
+    widgets::{
+        canvas::{Canvas, Map, MapResolution},
+        Block, BorderType, Borders, List, ListItem, Paragraph, Tabs, Widget,
+    },
+    DefaultTerminal,
+};
 //
 // Will decouple cloudflare reqwesting into library file later
 //
@@ -102,24 +116,111 @@ impl App {
             settings,
         }
     }
-}
 
-fn main() -> Result<(), std::io::Error> {
-    let terminal = ratatui::init();
-    let result = run(terminal);
-    ratatui::restore();
-    result
-}
-
-fn run(mut terminal: DefaultTerminal) -> Result<(), std::io::Error> {
-    loop {
-        terminal.draw(render)?;
-        if matches!(event::read()?, Event::Key(_)) {
-            break Ok(());
+    pub fn run(&self, mut terminal: DefaultTerminal) -> Result<(), std::io::Error> {
+        loop {
+            terminal.draw(|frame| {
+                frame.render_widget(self, frame.area());
+            });
+            if matches!(event::read()?, Event::Key(_)) {
+                break Ok(());
+            }
         }
     }
 }
 
-fn render(frame: &mut Frame) {
-    frame.render_widget("hello_world", frame.area());
+#[derive(Hash, Eq, PartialEq)]
+enum DisplayWidgetLayoutArea {
+    Navbar,
+    RequestQueue,
+    Map,
+    Settings,
+}
+
+impl Widget for &App {
+    fn render(self, area: Rect, buffer: &mut Buffer) {
+        let layouts: HashMap<DisplayWidgetLayoutArea, Rect> = {
+            let display_settings_layout = Layout::default()
+                .direction(Direction::Vertical)
+                .constraints(vec![Constraint::Percentage(80), Constraint::Percentage(20)])
+                .split(area);
+
+            let navdata_map_layout = Layout::default()
+                .direction(Direction::Horizontal)
+                .constraints(vec![Constraint::Percentage(30), Constraint::Percentage(70)])
+                .split(display_settings_layout[0]);
+
+            let nav_data_layout = Layout::default()
+                .direction(Direction::Vertical)
+                .constraints(vec![Constraint::Percentage(20), Constraint::Percentage(80)])
+                .split(navdata_map_layout[0]);
+
+            HashMap::from([
+                (DisplayWidgetLayoutArea::Navbar, nav_data_layout[0]),
+                (DisplayWidgetLayoutArea::RequestQueue, nav_data_layout[1]),
+                (DisplayWidgetLayoutArea::Map, navdata_map_layout[1]),
+                (
+                    DisplayWidgetLayoutArea::Settings,
+                    display_settings_layout[1],
+                ),
+            ])
+        };
+
+        let settings_block = Block::new().borders(Borders::ALL).title("Settings");
+        let settings_content = Paragraph::new(format!(
+            "Current Time Interval = {} minutes",
+            self.settings.time_interval.num_minutes()
+        ));
+        settings_content.block(settings_block).render(
+            *layouts.get(&DisplayWidgetLayoutArea::Settings).unwrap(),
+            buffer,
+        );
+
+        let map_block = Block::new().borders(Borders::ALL).title("Map");
+        let map_content = Canvas::default()
+            .marker(Marker::Braille)
+            .paint(|ctx| {
+                ctx.draw(&Map {
+                    color: Color::Green,
+                    resolution: MapResolution::High,
+                })
+            })
+            .x_bounds([-180.0, 180.0])
+            .y_bounds([-90.0, 90.0]);
+
+        map_content
+            .block(map_block)
+            .render(*layouts.get(&DisplayWidgetLayoutArea::Map).unwrap(), buffer);
+
+        let request_block = Block::new().borders(Borders::ALL).title("RequestQueue");
+        let request_content = List::new(vec![
+            ListItem::new("example1"),
+            ListItem::new("exmaple2"),
+            ListItem::new("e23"),
+        ]);
+        request_content.block(request_block).render(
+            *layouts.get(&DisplayWidgetLayoutArea::RequestQueue).unwrap(),
+            buffer,
+        );
+
+        let navbar_block = Block::new().borders(Borders::ALL).title("Navbar");
+        let navbar_content = Tabs::new(vec!["Tab1", "Tab2", "Tab3"])
+            .select(0)
+            .padding("", "")
+            .divider(" ");
+        navbar_content.block(navbar_block).render(
+            *layouts.get(&DisplayWidgetLayoutArea::Navbar).unwrap(),
+            buffer,
+        );
+    }
+}
+
+fn main() -> Result<(), std::io::Error> {
+    let terminal = ratatui::init();
+    let app = App::new(AppSettings {
+        time_interval: TimeDelta::minutes(360),
+    });
+    let result = app.run(terminal);
+    ratatui::restore();
+    result
 }
